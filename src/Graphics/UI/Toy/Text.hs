742fafc480218b0a688389cce9329a4d9f9054d0
Michael Sloan <mgsloan@gmail.com>
2012-12-04 00:19:40 -0800

    Add README

--- src/Graphics/UI/Toy/Text.hs
+++ src/Graphics/UI/Toy/Text.hs
@@ -360,6 +360,8 @@ instance (Eq m, Mark m, CanBeCursor m)
                    ===
                   ( strutX 10 ||| alignT (diagram mt) )
 
+-- A keyhandler, for use with @gtk-toy-diagrams@, which lets the user type
+-- characters, use the arrows, delete, and backspace. 
 textKeyHandler :: (Eq m, Mark m, CanBeCursor m)
                => KeyEvent -> MarkedText m -> MarkedText m
 textKeyHandler (True, ev) mt = case ev of
@@ -372,7 +374,6 @@ textKeyHandler (True, ev) mt = case ev of
     "End"       -> mutateCursors (const (maxIx, maxIx))
     "Delete"    -> editCursors (\(ivl, _) -> (second  (+1)  ivl, cursorText))
     "BackSpace" -> editCursors (\(ivl, _) -> (first (+(-1)) ivl, cursorText))
-    "Escape"    -> unsafePerformIO $ (quitToy >> return mt)
     _           -> mt
  where
   editCursors f = edit (whenMarked isCursor f) mt
@@ -380,9 +381,8 @@ textKeyHandler (True, ev) mt = case ev of
   insert s = editCursors $ second $ const (MarkedText s [((p, p), mkCursor)])
     where p = length s
 
-  mutateCursors f = mutateMarks
-                    ( \(i, m) -> if isCursor m then Just (f i, m) else Just (i, m) )
-                    mt
+  mutateCursors f
+    = mutateMarks ( \(i, m) -> Just (if isCursor m then f i else i, m) ) mt
 
   maxIx = textLength mt
 
